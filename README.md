4e9e6fc3507cc62d2f5938e39ecde69ecc69e55c
ErikFrankling <erik.frankling@frankling.se>
2023-11-06 21:12:38 +0100

    started ISA

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-instructions 
+instructions are 2 bits long
+
+| Opcode | Description |
+|--------|-------------|
+| 000X | Jump to memory adress X |
+| 10XY | If register X is equal to register Y, skip the next instruction |
+| 20XY | If register X is not equal to register Y, skip the next instruction |
+| 30XY | If register X is less than register Y, skip the next instruction |
+| 40XY | If register X is greater than register Y, skip the next instruction |
+| 5XNN | Add NN to register X |
+| 60XY | Set register X to register Y subtracted from register X |
+| 70XY | Set register X to register Y |
+| 8XNN | Set register X to the value of Y |
+
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,9 @@
 use std::{fs, process::{self, Stdio}, env::args, iter};
 
-#[repr(u8)]
-enum Instructions {
-    MOV,
-    ADD,
-    SUB,
-    MUL,
-    DIV,
-    JMP,
-    JE,
-    AND,
-    OR,
-    XOR,
-    NOT,
-}
-
 fn main() {
-    let mut registers = [0u8; 4];
+    let mut registers = [0u8; 16];
 
-    let mut memory = [0u8; 256];
+    let mut memory = [0u8; 4096];
 
     let path = args().nth(1).expect("No file path provided");
     let code = fs::read(&path).unwrap();
@@ -26,11 +11,12 @@ fn main() {
     println!("Source code:\n\n{:?}\n", code);
 
     code.iter().for_each(|byte| {
-        print!("{:02X} ", byte);
+        println!("{:02X} ", byte);
     });
-    for byte in code.iter() {
-        match byte {
-            _ => {}
+    for instruction in code.chunks(3) {
+        match instruction[0] {
+            
+            _ => {panic!("Unknown instruction: {:02X}", instruction[0])}
         }
     }
 }
